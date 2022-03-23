b60ba4bc13fa8f9f1f81f3952c0f63eced1b4839
caitlyn kidd <caitlyn.kidd92@gmail.com>
2022-03-22 18:07:14 -0600

    .

--- index.js
+++ index.js
@@ -7,13 +7,4 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes'));
-
-mongoose.connect('mongodb://localhost:27017/socialnetwork', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`Listening on Port:${PORT}`));
+app.use(require('./routes'))
